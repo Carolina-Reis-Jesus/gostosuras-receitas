d633005163526ec2576d7f0451b5979e025e40bb
Carolina Reis <crlnrjxxx@gmail.com>
2026-02-17 02:23:52 -0300

    terminei completamente a landing page desktop. parei nos ajustes do menu toggle hamburguer, e ainda quero fazer o carrosel de cards. mas no geral se eu não inventar moda ja esta pronto

--- script/script.js
+++ script/script.js
@@ -1,7 +1,11 @@
-// menu toggle
-const toggle = document.getElementById("menu-toggle");
-const nav = document.getElementById("nav");
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.getElementById('menu-toggle');
+    const nav = document.getElementById('nav');
 
-toggle.addEventListener("click", () => {
-    nav.classList.toggle("active");
+    if(menuToggle && nav) {
+        menuToggle.addEventListener('click', () => {
+            menuToggle.classList.toggle('open');
+            nav.classList.toggle('active');
+        });
+    }
 });
